af4ad56f5264c036daaa8dfe355d8e5c34a49b0a
Seonghyun Bae <qotjdgus320@gmail.com>
2022-01-19 06:53:11 +0900

    [SWEA] 10726 이진수 표현 주석 추가

--- SWEA/10726 이진수 표현/main.cpp
+++ SWEA/10726 이진수 표현/main.cpp
@@ -19,8 +19,10 @@ int main(int argc, char** argv)
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		cin >> N >> M;
-
+		// N비트가 모두 1인 수 계산
 		x = (1 << N) - 1;
+
+		// M의 뒤 N비트가 모두 1 이면
 		if ((M & x) == x)
 			printf("#%d ON\n", test_case);
 		else 
