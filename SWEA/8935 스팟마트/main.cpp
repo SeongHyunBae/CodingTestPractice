674550b7fe93ecd6f40dca09e781043d30665402
Seonghyun Bae <qotjdgus320@gmail.com>
2022-01-25 10:05:32 +0900

    [SWEA] 8935 스팟마트 수정

--- SWEA/8935 스팟마트/main.cpp
+++ SWEA/8935 스팟마트/main.cpp
@@ -8,64 +8,56 @@
 
 using namespace std;
 
-const int MAXA = 3002, MAXB = 102;
+const int MAXA = 3001, MAXB = 101;
 int N, M;
 
 int A[MAXA];
 int B[MAXB];
 int cache[MAXA][MAXB][MAXB][2];
 
-// a: 현재 가장 앞에 있는 A봉지 위치
-// b1: 현재 가장 많은 조각이 담긴 B봉지 위치 (왼쪽에서 오른쪽으로 이동)
-// b2: 현재 가장 적은 조각이 담긴 B봉지 위치 (오른쪽에서 왼쪽을 이동)
-// t: 1이면 선택 0이면 선택안함
-// cache: a, b1, b2, t 조건에서 가장 많은 조각을 가지는 값
-
-int dp(int a, int b1, int b2, int t)
+//dp[n_i][m_i][skip][get]
+//n_i : n봉지의 과자 중 n_i번째를 보고있는 상태(0과 n은 각각 왼쪽 끝, 오른쪽 끝을 의미한다.
+//m_i : m봉지의 과자 중 고른 과자의 수
+//skip : m봉지 중 스킵한 개수
+//get : 위 상태일 때 바로 직전에 과자를 get했는가 (했으면 1)
+int dp(int n_i, int m_i, int skip, int get)
 {
-	// A봉지, B봉지 모두 사용
-	if (a > N && b1 + (M - b2) > M) return 0;
+	// 만약 n봉지 끝까지 갔고 && m봉지도 끝까지 간 경우(고른 과자 + 스킵 과자)
+	if (n_i == N && m_i + skip == M) return 0;
 
-	// 캐시에서 값 가져오기
-	int& ret = cache[a][b1][b2][t];
-	// 같은 조건에서 이미 계산한 값이 있으면
+	int& ret = cache[n_i][m_i][skip][get];
 	if (ret != -1) return ret;
 
-	// 아래 모든 경우에 대해서 가장 많은 조각을 가지는 값을 리턴해야함
-	// 이번 과자 봉지를 선택안하는 경우
-	// -> 선택을 안하기 때문에 다음번에는 선택을 하는 경우 / 안하는 경우 모두 따져야함
-	if (t == 0) {
-		int nuseA = 0, nuseB1 = 0, nuseB2 = 0;
-		// A봉지가 있는 경우
-		if (a <= N)
-			nuseA = max(dp(a + 1, b1, b2, 0), dp(a + 1, b1, b2, 1));
+	ret = 0;
 
-		// B봉지가 있는 경우
-		if (b1 + (M - b2) <= M) {
-			// 가장 많은 조각이 담긴 B봉지를 가져가는 경우
-			nuseB1 = max(dp(a, b1 + 1, b2, 0), dp(a, b1 + 1, b2, 1));
-			// 가장 적은 조각이 담긴 B봉지를 가져가는 경우
-			nuseB2 = max(dp(a, b1, b2 - 1, 0), dp(a, b1, b2 - 1, 1));
+	// A봉지는 차례대로 놓고 고르고
+	// B봉지는 고를 때는 조각이 가장 많은 봉투, 고르지 않을 때는 조각이 가장 적은 봉투를 놓음
+
+	// 직전에 봉투를 고르지 않은 경우 -> 이번에는 고를수도 안고를수도 있음
+	if (get == 0) {
+		// 만약 n봉지를 다 고르지 않은 상태일 때
+		if (n_i < N) {
+			ret = max(ret, dp(n_i + 1, m_i, skip, 1) + A[n_i]); // 현재 차례의 A봉지를 놓고 고름
+			ret = max(ret, dp(n_i + 1, m_i, skip, 0)); // 현재 차례의 A봉지를 놓되 고르지 않음
 		}
-		ret = max({ nuseA, nuseB1, nuseB2 });
+
+		// 만약 m봉지를 다 고르지 않은 상태일 때
+		if (m_i + skip < M) {
+			ret = max(ret, dp(n_i, m_i + 1, skip, 1) + B[m_i]); // 과자가 가장 많은 B봉지를 놓고 고름
+			ret = max(ret, dp(n_i, m_i, skip + 1, 0)); // 과자가 가장 적은은 B봉지를 놓되 고르지 않음
+		}
 	}
-	// 이번 과자 봉지를 선택하는 경우
-	// -> 선택을 하기 때문에 가져간 조각의 개수 계산 필요 + 다음번에 선택하는 경우 제외
+	// 직전에 봉투를 고른 경우 -> 이번에는 고르지 못함
 	else {
-		int useA = 0, useB1 = 0, useB2 = 0;
-		// A봉지가 있는 경우
-		if (a <= N)
-			useA = dp(a + 1, b1, b2, 0) + A[a];
+		// 만약 n봉지를 다 고르지 않은 상태일 때
+		if (n_i < N)
+			ret = max(ret, dp(n_i + 1, m_i, skip, 0)); // 현재 차례의 A봉지를 놓되 고르지 않음(고르지 못함)
 
-		// B봉지가 있는 경우
-		if (b1 + (M - b2) <= M) {
-			// 가장 많은 조각이 담긴 B봉지를 가져가는 경우
-			useB1 = dp(a, b1 + 1, b2, 0) + B[b1];
-			// 가장 적은 조각이 담긴 B봉지를 가져가는 경우
-			useB2 = dp(a, b1, b2 - 1, 0) + B[b2];
-		}
-		ret = max({ useA , useB1, useB2 });
+		// 만약 m봉지를 다 고르지 않은 상태일 때
+		if (m_i + skip < M)
+			ret = max(ret, dp(n_i, m_i, skip + 1, 0)); // 과자가 가장 적은은 B봉지를 놓되 고르지 않음(고르지 못함)
 	}
+
 	return ret;
 }
 
@@ -81,24 +73,24 @@ int main()
 	for (int test_case = 1; test_case <= T; test_case++)
 	{
 		cin >> N;
-		for (int i = 1; i < N + 1; i++) cin >> A[i];
+		for (int i = 0; i < N; i++) cin >> A[i];
 
 		cin >> M;
-		for (int i = 1; i < M + 1; i++) cin >> B[i];
+		for (int i = 0; i < M; i++) cin >> B[i];
 
-		for (int i = 0; i <= N + 1; i++) {
-            for (int j = 0; j <= M + 1; j++) {
-                for (int k = 0; k <= M + 1; k++) {
+		for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= M; j++) {
+                for (int k = 0; k <= M; k++) {
                     cache[i][j][k][0] = -1;
                     cache[i][j][k][1] = -1;
                 }
             }
         }
 
-		// B 봉투를 내림 차순 정렬
-		sort(B, B + M);
+		// B 봉투를 내림차순 정렬
+		sort(B, B + M, greater<int>());
 
-		printf("#%d %d\n", test_case, max(dp(1, 1, M, 0), dp(1, 1, M, 1)));
+		printf("#%d %d\n", test_case, dp(0, 0, 0, 0));
 	}
 
 	return 0;
